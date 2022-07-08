b0654cb8c885ce28a46a8e7bfff2f7d96f8e64a2
whitehatjrvetrigithub <104361208+whitehatjrvetrigithub@users.noreply.github.com>
2022-07-08 18:28:03 +0530

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -3,14 +3,15 @@ from tkinter import filedialog
 from PIL import ImageTk, Image
 import os
 from tkinter import messagebox
+import webbrowser
 
 root = Tk()
 root.minsize(600, 600)
 root.maxsize(600, 600)
 
-open_img = ImageTk.PhotoImage(Image.open("open.png"))
-save_img = ImageTk.PhotoImage(Image.open("save.png"))
-run_img = ImageTk.PhotoImage(Image.open("exit.jpg"))
+open_img = ImageTk.PhotoImage(Image.open("open.jpg"))
+save_img = ImageTk.PhotoImage(Image.open("save.jpg"))
+run_img = ImageTk.PhotoImage(Image.open("run.jpg"))
 
 label_file_name = Label(root, text = "File Name :", bg = "white")
 label_file_name.place(relx = 0.40, rely = 0.05, anchor = CENTER)
@@ -36,14 +37,16 @@ def openFile():
     paragraph = html_file.read()
     my_text.insert(END, paragraph)
     html_file.close()
-
-open_button = Button(root, image = open_img, text = "Open File", command = openFile)
-open_button.place(relx = 0.05, rely = 0.05, anchor = CENTER)
-
-save_button = Button(root, image = save_img, text = "Save File")
-save_button.place(relx = 0.15, rely = 0.05, anchor = CENTER)
-
-run_button = Button(root, image = run_img, text = "Run File")
-run_button.place(relx = 0.23, rely = 0.05, anchor = CENTER)
-
-root.mainloop()
+    
+def save():
+    input_name = input_file_name.get()
+    file = open(input_name + ".html", "w")
+    data = my_text.get(1.0, END)
+    print(data)
+    file.write(data)
+    input_file_name.delete(0, END)
+    my_text.delete(1.0, END)
+    messagebox.showinfo("Update", "Success")
+    
+def run_html_file():
+    webbrowser.open(name)
